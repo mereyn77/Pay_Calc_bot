dd8e8ed0fd1116569947af0f8cf9e4c32b0a7f74
mereyn77 <omit28@mail.ru>
2024-12-07 01:37:02 +0300

    Create file, create sheets, create table header

---
+++ Svody/Svody.py
@@ -0,0 +1,128 @@
+# Программа для формирования файла сводов
+import os
+import openpyxl
+from openpyxl.styles import NamedStyle, Border, Side, Font, GradientFill, Alignment, PatternFill, Color
+from openpyxl.writer.excel import save_workbook
+from openpyxl import Workbook
+import pandas as pd
+import xlsxwriter
+import easygui
+import datetime
+from datetime import date
+
+#
+dt = datetime.datetime.now()
+dt2 = dt.strftime('%d %b %Y')
+month_list={'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06', 'Jul':'07',
+        'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
+month_num = int(month_list[dt2[3:6]])-1
+# dt3 = str(dt2[:3] + month_name + ' ' + dt2[7:13])
+dt3 = dt2.replace(dt2[3:9], str(month_num))
+dt4 = dt3[3:7]
+
+svodFile = 'Общ'+'_'+ dt4 + '.xlsx'
+
+# Lists of dpts and corresponding sheet names and column titles
+wb1 = openpyxl.load_workbook('Свод_образец.xlsx')
+ws_file_list = wb1['File_list']
+
+file_list = []
+file_list_length = ws_file_list.max_row
+dpt_list = []
+
+for f in range(1, file_list_length+1):
+        filename = ws_file_list.cell(row=f, column=2).value
+        file_list.append(filename)
+        dpt_name = ws_file_list.cell(row=f, column=1).value
+        if dpt_name == None:
+            continue
+        else:
+            dpt_list.append(dpt_name)
+
+ws_sample_table = wb1['Sample_table']
+col_titles = []
+sample_table_length = ws_sample_table.max_column
+
+for c in range(1, sample_table_length+1):
+    col_title = ws_sample_table.cell(row=1, column=c).value
+    col_titles.append(col_title)
+
+# Create Named Style for proper decoration of the table
+named_st1 = NamedStyle(name='tab_bord')
+named_st1.font = Font(name='Arial', bold=False, size=8)
+border = Side(style='thin', color='000000')
+named_st1.border = Border(left=border, top=border, right=border, bottom=border)
+
+# Create an Excel file for consolidating payrolls
+wb_Svody = Workbook()
+wb_Svody.add_named_style(named_st1)  # Registering the Named Style for further use
+
+# Generating column list
+columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD']
+gen_col = [g+'1' for g in columns]
+
+for sh in dpt_list:
+        ws = wb_Svody.create_sheet(title=sh)
+        ws.append(col_titles)
+        ws.freeze_panes = 'F2'
+        for a in gen_col:
+                ws[a].alignment = Alignment(wrap_text=True, vertical='center', horizontal='center')
+        for b in columns:
+                ws.column_dimensions[b].width=10
+        ws.merge_cells('D1:E1')
+        ws.row_dimensions[1].height=36
+        ws.column_dimensions['A'].width=5
+        ws.column_dimensions['B'].width=25
+        ws.column_dimensions['C'].width=13
+        ws.column_dimensions['D'].width=5
+        ws.column_dimensions['E'].width=7
+        # ws.column_dimensions['F'].width=8
+        # ws.column_dimensions['G'].width=7
+        # ws.column_dimensions['H'].width=8
+        # ws.column_dimensions['I'].width=8
+        ws.column_dimensions['J'].width=9
+        # ws.column_dimensions['K'].width=10
+        # ws.column_dimensions['L'].width=10
+        # ws.column_dimensions['M'].width=10
+        # ws.column_dimensions['N'].width=10
+        ws.column_dimensions['O'].width=12
+        # ws.column_dimensions['P'].width=10
+        ws.column_dimensions['Q'].width=11
+        ws.column_dimensions['R'].width=11
+        # ws.column_dimensions['S'].width=10
+        # ws.column_dimensions['T'].width=10
+        # ws.column_dimensions['U'].width=10
+        # ws.column_dimensions['V'].width=10
+        # ws.column_dimensions['W'].width=10
+        # ws.column_dimensions['X'].width=10
+        ws.column_dimensions['Y'].width=11
+        ws.column_dimensions['Z'].width=11
+        # ws.column_dimensions['AA'].width=10
+        # ws.column_dimensions['AB'].width=10
+        # ws.column_dimensions['AC'].width=10
+        # ws.column_dimensions['AD'].width=10
+
+
+
+
+wb_Svody.remove(wb_Svody['Sheet'])
+wb_Svody.save(svodFile)
+
+# ws_file_list = wb1.active('File_list')
+# file_list = []
+# file_list_length = ws_file_list.max_row
+
+
+
+# try:
+#     wbb4 = openpyxl.load_workbook(svodFile)
+# except:
+#     openpyxl.Workbook()
+# wsh4 = wbb4.create_sheet(period)
+# wbb4.active = wsh4
+
+print(dt2)
+print(dt3)
+print(svodFile)
+print(file_list_length)
+print(file_list)
